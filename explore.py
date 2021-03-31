fd2df257d99b4854e4c2c31f3a497807c6ef4e99
John Grinstead <john.allen.grinstead@gmail.com>
2021-03-30 22:55:56 -0500

    worked on modeling exercises

--- explore.py
+++ explore.py
@@ -261,3 +261,79 @@ def heat_map(df):
     q = sns.heatmap(df.corr(), cmap='RdYlBu', annot=True, center=0)
     return q
 
+###################### ________________________________________
+## Bivariate Categorical
+def explore_bivariate_categorical(train, categorical_target, continuous_target, binary):
+    '''
+    takes in binary categorical variable and binned/categorical target variable, 
+    returns a crosstab of frequencies
+    runs a chi-square test for the proportions
+    and creates a barplot, adding a horizontal line of the overall rate of the binary categorical variable. 
+    '''
+    print(binary, "\n_____________________\n")
+    ct = pd.crosstab(train[binary], train[categorical_target], margins=True)
+    chi2_summary, observed, expected = run_chi2(train, binary, categorical_target)
+    mannwhitney = compare_means(train, continuous_target, binary, alt_hyp='two-sided')
+    p = plot_cat_by_target(train, categorical_target, binary)
+    print("\nMann Whitney Test Comparing Means: ", mannwhitney)
+    print(chi2_summary)
+#     print("\nobserved:\n", ct)
+    print("\nexpected:\n", expected)
+    plt.show(p)
+    print("\n_____________________\n")
+def run_chi2(train, binary, categorical_target):
+    observed = pd.crosstab(train[binary], train[categorical_target])
+    chi2, p, degf, expected = stats.chi2_contingency(observed)
+    chi2_summary = pd.DataFrame({'chi2': [chi2], 'p-value': [p], 
+                                 'degrees of freedom': [degf]})
+    expected = pd.DataFrame(expected)
+    return chi2_summary, observed, expected
+def plot_cat_by_target(train, categorical_target, binary):
+    p = plt.figure(figsize=(2,2))
+    p = sns.barplot(categorical_target, binary, data=train, alpha=.8, color='lightseagreen')
+    overall_rate = train[binary].mean()
+    p = plt.axhline(overall_rate, ls='--', color='gray')
+    return p
+def compare_means(train, continuous_target, binary, alt_hyp='two-sided'):
+    x = train[train[binary]==0][continuous_target]
+    y = train[train[binary]==1][continuous_target]
+    return stats.mannwhitneyu(x, y, use_continuity=True, alternative=alt_hyp)
+###################### ________________________________________
+## Bivariate Quant
+def explore_bivariate_quant(train, categorical_target, continuous_target, quant):
+    '''
+    descriptive stats by each target class. 
+    compare means across 2 target groups 
+    boxenplot of target x quant
+    swarmplot of target x quant
+    '''
+    print(quant, "\n____________________\n")
+    descriptive_stats = train.groupby(categorical_target)[quant].describe().T
+    spearmans = compare_relationship(train, continuous_target, quant)
+    plt.figure(figsize=(4,4))
+    boxen = plot_boxen(train, categorical_target, quant)
+#     swarm = plot_swarm(train, categorical_target, quant)
+    plt.show()
+    scatter = plot_scatter(train, categorical_target, continuous_target, quant)
+    plt.show()
+    print(descriptive_stats, "\n")
+    print("\nSpearman's Correlation Test:\n", spearmans)
+    print("\n____________________\n")
+def compare_relationship(train, continuous_target, quant):
+    return stats.spearmanr(train[quant], train[continuous_target], axis=0)
+def plot_swarm(train, categorical_target, quant):
+    average = train[quant].mean()
+    p = sns.swarmplot(data=train, x=categorical_target, y=quant, color='lightgray')
+    p = plt.title(quant)
+    p = plt.axhline(average, ls='--', color='black')
+    return p
+def plot_boxen(train, categorical_target, quant):
+    average = train[quant].mean()
+    p = sns.boxenplot(data=train, x=categorical_target, y=quant, color='lightseagreen')
+    p = plt.title(quant)
+    p = plt.axhline(average, ls='--', color='black')
+    return p
+def plot_scatter(train, categorical_target, continuous_target, quant):
+    p = sns.scatterplot(x=quant, y=continuous_target, hue=categorical_target, data=train)
+    p = plt.title(quant)
+    return p
